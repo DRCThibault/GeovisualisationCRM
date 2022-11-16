6ad6a86573323e8431edaeeed6eca00b99277d13
DRCThibault <118354674+DRCThibault@users.noreply.github.com>
2022-11-16 13:51:28 +0100

    Update streamlit_geovisualisation_adho.py

--- streamlit_geovisualisation_adho.py
+++ streamlit_geovisualisation_adho.py
@@ -39,3 +39,39 @@ parties_menu = ["Centres d'intérêt",
                 "Meetings"]
 
 liste_categorie = st.sidebar.radio('', options=parties_menu)
+
+#CARTE DES PERSONNES INDIVIDUELLES
+carte_personnes = folium.Map(location=[47,2],zoom_start=6, tiles=None)
+
+base_map = folium.FeatureGroup(name='Basemap', overlay=True, control=False)
+folium.TileLayer().add_to(base_map)
+base_map.add_to(carte_personnes)
+
+def points_tag(tag):
+    r = lambda: random.randint(0,255)
+    couleur_choisie ='#%02X%02X%02X' % (r(),r(),r())
+    df_points = df[df[tag]=='Oui'] 
+    groupes = MarkerCluster()
+    for i in range(df_points.shape[0]):
+        message = "<strong>" + df_points['first_name'].iloc[i] + ' ' + df_points['last_name'].iloc[i] + "</strong>"
+        for j in range(df_points.shape[1]):
+            if df_points.iloc[i,j]=='Oui':
+                message += "<br>" + df_points.columns[j]
+        
+        latitude = df_points['Latitude'].iloc[i]
+        longitude = df_points['Longitude'].iloc[i]
+
+        mk = folium.CircleMarker([latitude, longitude], radius = 5, tooltip = message, 
+                        color=None, fill_color =couleur_choisie, fill_opacity=1)
+        groupes.add_child(mk)
+        groupes.add_to(feature_group)
+    feature_group.add_to(carte_personnes)
+
+for tag in liste_categorie :
+    feature_group = folium.FeatureGroup(name = tag, overlay=False)
+    points_tag(tag)
+
+
+folium.LayerControl().add_to(carte_personnes)
+
+folium_static(carte_personnes) 
